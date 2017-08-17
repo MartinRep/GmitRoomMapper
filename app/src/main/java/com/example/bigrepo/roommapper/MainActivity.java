ed90c4099da17cf0837c08d3cb72da90b3814532
repickym <repickym@gmail.com>
2017-08-17 14:37:46 +0100

    readFromFile

--- app/src/main/java/com/example/bigrepo/roommapper/MainActivity.java
+++ app/src/main/java/com/example/bigrepo/roommapper/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bigrepo.roommapper;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Environment;
 import android.support.annotation.IdRes;
 import android.support.v4.app.ActivityCompat;
@@ -14,9 +15,12 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 
@@ -48,9 +52,17 @@ public class MainActivity extends AppCompatActivity {
         submitButton = (Button) findViewById(R.id.buttonSubmit);
         radioGroup = (RadioGroup) findViewById(R.id.roomConnectionRadioId);
 
+        logOutput.setBackgroundColor(Color.BLACK);
+        logOutput.setTextColor(Color.GREEN);
+
         //Checks for storage permissions and ask for then if needed
         if(!verifyStoragePermissions(MainActivity.this)){
             //display error message for lack of storage permission
+        }
+        try {
+            readFromFile();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -87,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
                 corridorNumber = corridorNumberId.getText().toString();
                 if(!roomNumber.toString().equals("") && !corridorNumber.toString().equals("") && direction.length > 1) {
                     StringBuilder unitToSave = new StringBuilder();
+                    logOutput.append("Room "+roomNumber+" added.\n");
                     unitToSave.append("CREATE ("+roomNumber+")-[:Access{connection:['"+direction[0]+"']}]->("+corridorNumber+")\n");
                     unitToSave.append("CREATE ("+corridorNumber+")-[:Access{connection:['"+direction[1]+"']}]->("+roomNumber+")\n");
                     data.add(unitToSave.toString());
@@ -97,16 +110,31 @@ public class MainActivity extends AppCompatActivity {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                    roomNumberId.setText("");
                 }
             }
         });
+    }
+
+    private void readFromFile() throws IOException{
+        String line;
+        int dataRead = 0;
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName);
+        FileInputStream inFile = new FileInputStream(file);
+        InputStreamReader inStream = new InputStreamReader(inFile);
+        BufferedReader inData = new BufferedReader(inStream);
+        while ((line = inData.readLine()) != null) {
+            data.add(line+"\n");
+            dataRead++;
+        }
+        logOutput.append("Data read from file: "+dataRead);
     }
 
     private void saveToFile() throws IOException {
         StringBuilder dataToSave = new StringBuilder();
         for (String s : data) {
             dataToSave.append(s);
-            dataToSave.append("\n");
+            //dataToSave.append("\n");
         }
         File file = new File(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_DOWNLOADS), fileName);
